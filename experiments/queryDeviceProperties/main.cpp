0e708577f05a2cd5d4515b00075029efe50858fb
moddyz <moddy3d@gmail.com>
2020-05-07 19:47:18 +1200

    Documented some properties, separated properties into categories.

--- experiments/queryDeviceProperties/main.cpp
+++ experiments/queryDeviceProperties/main.cpp
@@ -1,15 +1,21 @@
 #include <cstdio>
 #include <stdlib.h>
+#include <string>
 
 #include <cuda_runtime.h>
 
 #include <cudaUtils.h>
 
-/// Utility program for querying all CUDA devices, and their respective properties.
+/// Utility program for querying available CUDA devices, and their respective properties.
+
+static const char* GetEnabledToken( bool i_status )
+{
+    return i_status ? "Enabled" : "Disabled";
+}
 
 int main()
 {
-    printf( "[cudaQueryDeviceProperties]\n\n" );
+    printf( "[cudaQueryDeviceProperties]\n" );
 
     int deviceCount = 0;
     CUDA_CHECK_ERROR_FATAL( cudaGetDeviceCount( &deviceCount ) );
@@ -21,18 +27,50 @@ int main()
         cudaDeviceProp deviceProp;
         cudaGetDeviceProperties( &deviceProp, deviceIndex );
 
-        printf( "\nDevice %d: \"%s\"\n", deviceIndex, deviceProp.name );
+        printf( "\nDevice %d: \"%s\"\n\n", deviceIndex, deviceProp.name );
 
+        //
+        // Overview
+        //
+
+        printf( "  Overview\n" );
+
+        // The latest version of CUDA supported by the driver.
         int driverVersion = 0;
-        int runtimeVersion = 0;
         cudaDriverGetVersion( &driverVersion );
+
+        // The version of the CUDA libraries linked into this runtime.
+        int runtimeVersion = 0;
         cudaRuntimeGetVersion( &runtimeVersion );
-        printf( "  CUDA Driver Version / Runtime Version          %d.%d / %d.%d\n",
+        printf( "    CUDA Driver Version / Runtime Version          %d.%d / %d.%d\n",
                 driverVersion / 1000,
                 ( driverVersion % 100 ) / 10,
                 runtimeVersion / 1000,
                 ( runtimeVersion % 100 ) / 10 );
-        printf( "  CUDA Capability Major/Minor version number:    %d.%d\n", deviceProp.major, deviceProp.minor );
+
+        // CUDA Device Capability.
+        printf( "    CUDA Capability Major/Minor version number:    %d.%d\n", deviceProp.major, deviceProp.minor );
+        printf( "\n" );
+
+        //
+        // Memory
+        //
+
+        printf( "  Memory\n" );
+
+        printf( "    Total amount of global memory:                 %.0f MBytes\n",
+                static_cast< float >( deviceProp.totalGlobalMem / ( /* MBytes -> Bytes */ 1024.0f * 1024.0f ) ) );
+        printf( "    Total amount of constant memory:               %.0f Bytes\n",
+                static_cast< float >( deviceProp.totalConstMem ) );
+
+        printf( "\n" );
+
+        // Device features
+        printf( "  Device Features\n" );
+
+        // ECC == Error-Correcting code memory.  It is a type of storage which can detect and correct most
+        // common kinds of internal data corruption.
+        printf( "    ECC Support:                                   %s\n", GetEnabledToken( deviceProp.ECCEnabled ) );
     }
 
     return EXIT_SUCCESS;
