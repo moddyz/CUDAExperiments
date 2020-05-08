fa5549e059f33a6614fb066fe3be7f1724510ec2
moddyz <moddy3d@gmail.com>
2020-05-08 19:52:33 +1200

    Fixed MAP header docstring.

--- experiments/matrixArrayOps/matrixArrayProduct.h
+++ experiments/matrixArrayOps/matrixArrayProduct.h
@@ -1,10 +1,13 @@
 #pragma once
 
-/// \page MatrixArrayProduct Matrix Array Product
+/// \file matrixArrayProduct.h
+///
+/// Matrix array product
 ///
 /// Element-wise multiplication of two arrays of 4 x 4 matrices (commonly used in computer graphics).
 
 #include "valueTypes.h"
 
+/// Naive implementation of MAP (matrix array product)
 __global__ void
 MatrixArrayProduct_Naive( const Mat4f* i_matricesA, const Mat4f* i_matricesB, int i_numMatrices, Mat4f* o_matrices );
