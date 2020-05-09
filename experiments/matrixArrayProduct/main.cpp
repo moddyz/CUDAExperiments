a54dec7067f5c4bcc75c65b2b3081252ce73fbab
moddyz <moddy3d@gmail.com>
2020-05-09 22:45:55 +1200

    Added some simple benchmarking tools.

--- experiments/matrixArrayProduct/main.cpp
+++ experiments/matrixArrayProduct/main.cpp
@@ -6,8 +6,8 @@
 
 // Local tools.
 #include <cudaExperiments/error.h>
-#include <cudaExperiments/performance.h>
 #include <cudaExperiments/math.h>
+#include <cudaExperiments/performance.h>
 #include <cudaExperiments/valueTypes.h>
 
 // Thirdparty.
@@ -46,16 +46,19 @@ int main( int i_argc, char** i_argv )
     cxxopts::Options options( "cudaMatrixArrayOps",
                               "Testing various implementations of 4 by 4 matrix array operations." );
     options.add_options()( "n,arraySize", "Size of matrix array.", cxxopts::value< int >()->default_value( "10000" ) );
-    auto result = options.parse( i_argc, i_argv );
-    int arraySize = result[ "arraySize" ].as< int >();
+    auto result    = options.parse( i_argc, i_argv );
+    int  arraySize = result[ "arraySize" ].as< int >();
 
     // Compute amount of memory to allocate.
     size_t numBytes = arraySize * sizeof( Mat4f );
 
+    // Print current device perf attributes.
+    CudaPrintDevicePerformanceAttributes();
+
     // Allocate host memory.
-    Mat4f* matricesA = ( Mat4f* ) malloc( numBytes );
-    Mat4f* matricesB = ( Mat4f* ) malloc( numBytes );
-    Mat4f* matricesC = ( Mat4f* ) malloc( numBytes );
+    Mat4f* matricesA   = ( Mat4f* ) malloc( numBytes );
+    Mat4f* matricesB   = ( Mat4f* ) malloc( numBytes );
+    Mat4f* matricesC   = ( Mat4f* ) malloc( numBytes );
     Mat4f* matricesRef = ( Mat4f* ) malloc( numBytes );
 
     // Set host values.
@@ -80,39 +83,18 @@ int main( int i_argc, char** i_argv )
         cudaMemcpy( /*dst*/ matricesBDevice, /*src*/ matricesB, numBytes, cudaMemcpyHostToDevice ) );
 
     // Compute grid & block size based on array size.
-    dim3 blockSize, gridSize;
-    blockSize.x = 256;
-    gridSize.x = ( arraySize + blockSize.x - 1 ) / blockSize.x;
+    {
+        CudaKernelLaunchParams params;
+        params.name    = "MatrixArrayProduct_Naive";
+        params.kernel  = ( void* ) MatrixArrayProduct_Naive;
+        params.block.x = 256;
+        params.grid.x  = ( arraySize + params.block.x - 1 ) / params.block.x;
+        params.args    = {&matricesADevice, &matricesBDevice, &arraySize, &matricesCDevice};
+        CudaKernelBenchmark( params, /* bytesRead */ numBytes * 2, /* bytesWritten */ numBytes );
+    }
 
-    // Create events for timings.
-    cudaEvent_t start, stop;
-    CUDA_CHECK_ERROR_FATAL( cudaEventCreate( &start ) );
-    CUDA_CHECK_ERROR_FATAL( cudaEventCreate( &stop ) );
-
-    // Start timer.
-    CUDA_CHECK_ERROR_FATAL( cudaEventRecord( start, 0 ) );
-
-    // Execute kernel.
-    void* args[] = {&matricesADevice, &matricesBDevice, &arraySize, &matricesCDevice};
-    CUDA_CHECK_ERROR_FATAL(
-        cudaLaunchKernel( ( void* ) MatrixArrayProduct_Naive, gridSize, blockSize, args, 0, nullptr ) );
-
-    // Stop timer, and get elapsed time in milliseconds.
-    CUDA_CHECK_ERROR_FATAL( cudaEventRecord( stop, 0 ) );
-    CUDA_CHECK_ERROR_FATAL( cudaEventSynchronize( stop ) );
-    float elapsedMs;
-    CUDA_CHECK_ERROR_FATAL( cudaEventElapsedTime( &elapsedMs, start, stop ) );
-
-    // Print results.
-    printf( "MatrixArrayProduct_Naive:\n" );
-    printf( "   Elapsed time:                   %f ms\n", elapsedMs );
-    printf( "   Theoretical Bandwidth:          %f GB/s\n", CudaComputeTheoreticalMemoryBandwidth() );
-    printf( "   Effective Bandwidth:            %f GB/s\n",
-            CudaComputeEffectiveMemoryBandwidth( numBytes * 2, numBytes, elapsedMs ) );
-
-    // Download computed matrices.
+    // Download computed matrices, and verify against CPU results.
     CUDA_CHECK_ERROR_FATAL(
         cudaMemcpy( /*dst*/ matricesC, /*src*/ matricesCDevice, numBytes, cudaMemcpyDeviceToHost ) );
-
     CheckMatrixArrays( matricesC, matricesRef, arraySize );
 }
--- lib/cudaExperiments/performance.h
+++ lib/cudaExperiments/performance.h
@@ -6,6 +6,8 @@
 
 #include <cudaExperiments/error.h>
 
+#include <vector>
+
 /// Compute the theoretical memory bandwidth of the current CUDA device.
 /// The bandwidth proportional to memory clock rate and bandwidth.
 ///
@@ -37,4 +39,65 @@ inline double CudaComputeEffectiveMemoryBandwidth( size_t i_numBytesRead, size_t
     return bytesPerSecond / 1e9;
 }
 
+/// Prints performance attributes specific to the current CUDA device.
+inline void CudaPrintDevicePerformanceAttributes()
+{
+    int currentDeviceIndex;
+    CUDA_CHECK_ERROR_FATAL( cudaGetDevice( &currentDeviceIndex ) );
+    cudaDeviceProp deviceProp;
+    cudaGetDeviceProperties( &deviceProp, currentDeviceIndex );
+    printf( "\n[CUDA Device %d: \"%s\"]\n", currentDeviceIndex, deviceProp.name );
+    printf( "   Theoretical Memory Bandwidth:           %f GB/s\n", CudaComputeTheoreticalMemoryBandwidth() );
+    printf( "\n" );
+}
 
+/// \struct CudaKernelLaunchParams
+///
+/// Stores parameters required launch a CUDA kernel.
+struct CudaKernelLaunchParams
+{
+    const char*          name   = nullptr; /// Name of the kernel.
+    void*                kernel = nullptr; /// Pointer to the kernel function.
+    std::vector< void* > args;             /// Args the arguments to the kernel function.
+    dim3                 grid;             /// The block launch dimensions.
+    dim3                 block;            /// The thread launch dimensions.
+};
+
+/// Execute a kernel benchmark.
+/// The kernel is timed, and the effective memory bandwidth is computed.
+/// Results are printed to stdout.
+///
+/// \param i_kernelParams cuda kernel launch parameters.
+/// \param i_numBytesRead number of bytes of memory read in the kernel, used for computing effective bandwidth.
+/// \param i_numBytesWritten number of bytes of memory written in the kernel, used for computing effective bandwidth.
+inline void
+CudaKernelBenchmark( CudaKernelLaunchParams& i_kernelParams, size_t i_numBytesRead, size_t i_numBytesWritten )
+{
+    // Create events for timings.
+    cudaEvent_t start, stop;
+    CUDA_CHECK_ERROR_FATAL( cudaEventCreate( &start ) );
+    CUDA_CHECK_ERROR_FATAL( cudaEventCreate( &stop ) );
+
+    // Start timer.
+    CUDA_CHECK_ERROR_FATAL( cudaEventRecord( start, 0 ) );
+
+    // Execute kernel.
+    CUDA_CHECK_ERROR_FATAL( cudaLaunchKernel( i_kernelParams.kernel,
+                                              i_kernelParams.grid,
+                                              i_kernelParams.block,
+                                              i_kernelParams.args.data(),
+                                              0,
+                                              nullptr ) );
+
+    // Stop timer, and get elapsed time in milliseconds.
+    CUDA_CHECK_ERROR_FATAL( cudaEventRecord( stop, 0 ) );
+    CUDA_CHECK_ERROR_FATAL( cudaEventSynchronize( stop ) );
+    float elapsedMs;
+    CUDA_CHECK_ERROR_FATAL( cudaEventElapsedTime( &elapsedMs, start, stop ) );
+
+    // Print results.
+    printf( "<<< Kernel Benchmark \"%s\" >>>\n", i_kernelParams.name );
+    printf( "   Elapsed time:                           %f ms\n", elapsedMs );
+    printf( "   Effective Bandwidth:                    %f GB/s\n",
+            CudaComputeEffectiveMemoryBandwidth( i_numBytesRead, i_numBytesWritten, elapsedMs ) );
+}
