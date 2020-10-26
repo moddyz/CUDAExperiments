a5bd2d3205f66de45d55fae1aaf32c090f7ba780
moddyz <moddy3d@gmail.com>
2020-10-26 14:31:50 +1300

    Persisting L2 example WIP

---
+++ src/persistingL2/main.cu
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+#include <cudaTools/error.h>
+
+int main( int i_argc, char** i_argv )
+{
+    // Create a cuda stream.
+    cudaStream_t stream;
+    CUDA_CHECK( cudaStreamCreate( &stream ) );
+    
+    // Get device count. 
+    int deviceCount = 0;
+    CUDA_CHECK( cudaGetDeviceCount( &deviceCount ) );
+
+    for ( int deviceIndex = 0; deviceIndex < deviceCount; ++deviceIndex )
+    {
+        // Get cuda device properties.
+        cudaDeviceProp prop;
+        cudaGetDeviceProperties( &prop, deviceIndex );
+        
+        // Query total capacity of L2 cache.
+        printf("l2CacheSize: %f MB\n", (float)prop.l2CacheSize / 1024.0f );
+        
+        // Set persisting cache size.
+        size_t size = min( int( prop.l2CacheSize * 0.75 ), prop.persistingL2CacheMaxSize );
+        cudaDeviceSetLimit( cudaLimitPersistingL2CacheSize, size );
+
+        // TODO
+    }
+}
